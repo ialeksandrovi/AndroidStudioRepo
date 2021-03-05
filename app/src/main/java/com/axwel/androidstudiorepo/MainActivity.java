a75f07a1b91b163d7a51f3c98837d6155eb47afd
ialeksandrovi <ialexandrov@dejavoosystems.ru>
2021-03-05 23:26:22 +0700

    По-быстрому накидал чтобы соотвествовало минимальным требованиям.

--- app/src/main/java/com/axwel/androidstudiorepo/MainActivity.java
+++ app/src/main/java/com/axwel/androidstudiorepo/MainActivity.java
@@ -2,13 +2,30 @@ package com.axwel.androidstudiorepo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.tvTextView);
+        textView.setText(R.string.text1);
+
+        EditText editText = findViewById(R.id.etEditText);
+        editText.setBackgroundColor(R.color.color1);
+
+        Button button = findViewById(R.id.btnButton);
+        button.setText(R.string.go_to_second_screen);
+        button.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SecondActivity.class)));
     }
 }
---
+++ app/src/main/java/com/axwel/androidstudiorepo/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.axwel.androidstudiorepo;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity_layout);
+    }
+}
